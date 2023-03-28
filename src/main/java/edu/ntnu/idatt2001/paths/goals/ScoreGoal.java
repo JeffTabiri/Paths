a689cb460b0255335a891bf60cc3dc0c9eefde74
Elementum <tabirijeffrey@gmail.com>
2023-03-28 14:14:02 +0200

    refactor: refactor tests

--- src/main/java/edu/ntnu/idatt2001/paths/goals/ScoreGoal.java
+++ src/main/java/edu/ntnu/idatt2001/paths/goals/ScoreGoal.java
@@ -1,38 +1,31 @@
 package edu.ntnu.idatt2001.paths.goals;
 
 import edu.ntnu.idatt2001.paths.Player;
-import java.util.HashSet;
-import java.util.List;
 
-/**
- * An Action represents a future change in the state of a player,
- * including changes in the player's score, health, gold inventory, or inventory.
- *
- * @author Created by Jeffrey Yaw Annor Tabiri and Ari Maman
- * @version 06/02/2023
- * @since JDK 17.0.6
- */
 public class ScoreGoal implements Goal {
-    private final int score;
+  private final int score;
 
-    /**
-     * The constructor for the class {@code InventoryGoal}.
-     *
-     * @param mandatoryItems is a list of items that the player must have to reach the goal.
-     */
-    public ScoreGoal(int score) {
-        this.score = score;
+  /**
+   * The constructor for the class ScoreGoal.
+   *
+   * @param score the score to be reached. It is a positive integer.
+   * @throws IllegalArgumentException if the score is negative.
+   */
+  public ScoreGoal(int score) {
+    if (score < 0) {
+      throw new IllegalArgumentException("The score cannot be negative");
     }
+    this.score = score;
+  }
 
     /**
-     * The method {@code isFulfilled} checks if the player has reached the goal.
-     *
-     * @return true if the player has reached the goal, false otherwise.
+     * The method isFulfilled checks if the player has reached the goal.
+     * @param player the player to check if the goal is fulfilled.
+     * @return true if the player has reached the goal, false if not.
      */
+    @Override
     public boolean isFulfilled(Player player) {
-        return score >= player.getScore();
+        return player.getScore() >= score;
     }
 
 }
-
-
