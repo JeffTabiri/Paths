af2723d42511aa1002d54e0cdf7c0d5d034b15c5
Jeffrey Yaw Annor Tabiri <jytabiri@stud.ntnu.no>
2023-04-13 03:07:38 +0200

    Revert "Merge branch '14-addition/make_a_story' into 'master'"
    This reverts merge request !11

---
+++ src/test/java/edu/ntnu/idatt2001/paths/goals/GoalTest.java
@@ -0,0 +1,14 @@
+package edu.ntnu.idatt2001.paths.goals;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GoalTest {
+
+    @Test
+    void isFulfilled() {
+
+    }
+}
