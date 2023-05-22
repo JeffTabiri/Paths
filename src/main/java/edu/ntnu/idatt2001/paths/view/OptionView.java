fcb26e84fd474e7f8e2acc65c546291d499c0a0f
Elementum <tabirijeffrey@gmail.com>
2023-05-22 15:08:48 +0200

    docs(option-scene): add javadoc and reformat

--- src/main/java/edu/ntnu/idatt2001/paths/view/OptionView.java
+++ src/main/java/edu/ntnu/idatt2001/paths/view/OptionView.java
@@ -1,8 +1,9 @@
 package edu.ntnu.idatt2001.paths.view;
 
+import edu.ntnu.idatt2001.paths.controller.ChooseStoryController;
 import edu.ntnu.idatt2001.paths.controller.OptionController;
-import edu.ntnu.idatt2001.paths.model.OptionManager;
-import edu.ntnu.idatt2001.paths.utility.ButtonEffects;
+import edu.ntnu.idatt2001.paths.model.manager.OptionManager;
+import edu.ntnu.idatt2001.paths.utility.ButtonUtility;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
@@ -17,29 +18,52 @@ import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 import org.controlsfx.control.ToggleSwitch;
 
+/**
+ * <h1>OptionView</h1>
+ * {@code OptionView} is the view class for the option view.
+ * This class is responsible for building the option view.
+ *
+ * @author Elementum
+ * @version 1.0
+ * @see ChooseStoryController
+ * @since 06/04/2023
+ */
 public class OptionView {
+  private final BorderPane root;
+  private final ToggleSwitch darkModeButton;
 
-  BorderPane root = new BorderPane();
-  ToggleSwitch darkModeButton = new ToggleSwitch();
-  ToggleSwitch soundButton = new ToggleSwitch();
-  ToggleSwitch fullscreenButton = new ToggleSwitch();
+  private final ToggleSwitch fullscreenButton;
+  private final OptionController controller;
+  private final OptionManager optionManager;
 
-  OptionController controller;
-
-  OptionManager optionManager = OptionManager.getInstance();
-
-
+  /**
+   * Constructor for the view. Responsible for building the view.
+   *
+   * @param controller the controller for the view.
+   */
   public OptionView(OptionController controller) {
+
+    optionManager = OptionManager.getInstance();
+    root = new BorderPane();
+    darkModeButton = new ToggleSwitch();
+
+    fullscreenButton = new ToggleSwitch();
     this.controller = controller;
+
     buildView();
   }
 
 
-  public void buildView() {
+  /**
+   * Responsible for building the view.
+   */
+  private void buildView() {
+
     root.setTop(buildTitle());
     root.setCenter(buildMenu());
     root.setBottom(buildBottomMenu());
-    root.getStylesheets().add(optionManager.getCurrentStyle());
+    root.getStylesheets().add(optionManager.getCurrentStyleSheet());
+
   }
 
   public Parent asParent() {
@@ -47,158 +71,142 @@ public class OptionView {
   }
 
 
-    private HBox buildTitle() {
+  private HBox buildTitle() {
 
-      //Title container
-      HBox titleContainer = new HBox();
+    //Title container
+    HBox titleContainer = new HBox();
 
-      titleContainer.setPadding(new Insets(10, 10, 10, 10));
+    titleContainer.setPadding(new Insets(10, 10, 10, 10));
 
-      //Title
-      Label title = new Label("Options");
+    //Title
+    Label title = new Label("Options");
 
-      titleContainer.getChildren().add(title);
+    titleContainer.getChildren().add(title);
 
-      HBox.setHgrow(title, Priority.ALWAYS);
+    HBox.setHgrow(title, Priority.ALWAYS);
 
-      titleContainer.setAlignment(Pos.CENTER);
+    titleContainer.setAlignment(Pos.CENTER);
 
-      //Title styling
-      title.getStyleClass().add("title");
+    //Title styling
+    title.getStyleClass().add("title");
 
-      return titleContainer;
-    }
-
-    private VBox buildMenu() {
-
-      VBox menuContainer = new VBox();
-
-
-      //Dark mode container
-      HBox rightDarkModeContainer = new HBox();
-      HBox leftDarkModeContainer = new HBox();
-      HBox darkModeContainer = new HBox();
-
-
-      darkModeButton = new ToggleSwitch();
-      darkModeButton.setSelected(optionManager.getDarkMode());
-
-      TextFlow darkModeText = new TextFlow(new Text("Dark Mode "));
-
-
-      rightDarkModeContainer.getChildren().add(darkModeButton);
-      leftDarkModeContainer.getChildren().add(darkModeText);
-      darkModeContainer.getChildren().addAll(leftDarkModeContainer, rightDarkModeContainer);
-
-      HBox.setHgrow(leftDarkModeContainer, Priority.ALWAYS);
-      HBox.setHgrow(rightDarkModeContainer, Priority.ALWAYS);
-
-      rightDarkModeContainer.setAlignment(Pos.CENTER_RIGHT);
-      leftDarkModeContainer.setAlignment(Pos.CENTER_LEFT);
-
-      //Sound container
-      HBox rightSoundContainer = new HBox();
-      HBox leftSoundContainer = new HBox();
-      HBox soundContainer = new HBox();
-
-      soundButton = new ToggleSwitch();
-      soundButton.setSelected(optionManager.getSound());
-
-
-      TextFlow soundText = new TextFlow(new Text("Mute: "));
-      rightSoundContainer.getChildren().add(soundButton);
-      leftSoundContainer.getChildren().add(soundText);
-      soundContainer.getChildren().addAll(leftSoundContainer, rightSoundContainer);
-
-      HBox.setHgrow(leftSoundContainer, Priority.ALWAYS);
-      HBox.setHgrow(rightSoundContainer, Priority.ALWAYS);
-
-      rightSoundContainer.setAlignment(Pos.CENTER_RIGHT);
-      leftSoundContainer.setAlignment(Pos.CENTER_LEFT);
-
-
-      //Fullscreen container
-      HBox rightFullscreenContainer = new HBox();
-      HBox leftFullscreenContainer = new HBox();
-      HBox fullscreenContainer = new HBox();
-
-      fullscreenButton = new ToggleSwitch();
-      fullscreenButton.setSelected(optionManager.getFullscreen());
-
-
-      TextFlow fullscreenText = new TextFlow(new Text("Fullscreen:"));
-      rightFullscreenContainer.getChildren().add(fullscreenButton);
-      leftFullscreenContainer.getChildren().add(fullscreenText);
-      fullscreenContainer.getChildren().addAll(leftFullscreenContainer, rightFullscreenContainer);
-
-
-      HBox.setHgrow(leftFullscreenContainer, Priority.ALWAYS);
-      HBox.setHgrow(rightFullscreenContainer, Priority.ALWAYS);
-      rightFullscreenContainer.setAlignment(Pos.CENTER_RIGHT);
-      leftFullscreenContainer.setAlignment(Pos.CENTER_LEFT);
-
-      menuContainer.getChildren().addAll(darkModeContainer, soundContainer, fullscreenContainer);
-
-
-      //Menu styling
-      menuContainer.getStyleClass().add("menu");
-
-      return menuContainer;
-    }
-
-    private HBox buildBottomMenu() {
-
-      //Bottom menubar container
-      HBox leftBox = new HBox();
-      HBox rightBox = new HBox();
-      HBox bottom = new HBox();
-
-      //Buttons
-      Button returnButton = new Button("Return");
-      Button saveButton = new Button("Save");
-
-
-      //Padding
-      leftBox.setPadding(new Insets(20, 0, 20, 20));
-      rightBox.setPadding(new Insets(20, 20, 20, 0));
-
-      returnButton.setOnMouseEntered(event -> ButtonEffects.buttonHover(returnButton));
-      saveButton.setOnMouseEntered(event -> ButtonEffects.buttonHover(saveButton));
-
-      returnButton.setOnMouseExited(event -> ButtonEffects.buttonExit(returnButton));
-      saveButton.setOnMouseExited(event -> ButtonEffects.buttonExit(saveButton));
-
-
-      //Button placement
-      leftBox.getChildren().add(returnButton);
-      rightBox.getChildren().add(saveButton);
-      bottom.getChildren().addAll(leftBox, rightBox);
-
-      //Node alignment
-      leftBox.setAlignment(Pos.CENTER_LEFT);
-      rightBox.setAlignment(Pos.CENTER_RIGHT);
-
-      //Node padding
-      HBox.setHgrow(leftBox, Priority.ALWAYS);
-      HBox.setHgrow(rightBox, Priority.ALWAYS);
-
-      saveButton.setOnAction(event -> {
-        controller.saveSettings(darkModeButton.isSelected(), soundButton.isSelected(), fullscreenButton.isSelected());
-        Stage stage = (Stage) saveButton.getScene().getWindow();
-        stage.close();
-      });
-
-      returnButton.setOnAction(event -> {
-        controller.returnHandler();
-      });
-
-      //Styling
-      returnButton.getStyleClass().add("secondary-button");
-      saveButton.getStyleClass().add("secondary-button");
-
-      return bottom;
-    }
-
-
+    return titleContainer;
   }
+
+  private VBox buildMenu() {
+
+    final VBox menuContainer = new VBox();
+
+
+    /*
+     Dark mode container
+     */
+
+    final HBox rightDarkModeContainer = new HBox();
+    final HBox leftDarkModeContainer = new HBox();
+    final HBox darkModeContainer = new HBox();
+
+    darkModeButton.setSelected(optionManager.getIsDarkMode());
+
+    TextFlow darkModeText = new TextFlow(new Text("Dark Mode "));
+
+
+    rightDarkModeContainer.getChildren().add(darkModeButton);
+    leftDarkModeContainer.getChildren().add(darkModeText);
+    darkModeContainer.getChildren().addAll(leftDarkModeContainer, rightDarkModeContainer);
+
+    HBox.setHgrow(leftDarkModeContainer, Priority.ALWAYS);
+    HBox.setHgrow(rightDarkModeContainer, Priority.ALWAYS);
+
+    rightDarkModeContainer.setAlignment(Pos.CENTER_RIGHT);
+    leftDarkModeContainer.setAlignment(Pos.CENTER_LEFT);
+
+
+    //Fullscreen container
+    final HBox rightFullscreenContainer = new HBox();
+    final HBox leftFullscreenContainer = new HBox();
+    final HBox fullscreenContainer = new HBox();
+
+    fullscreenButton.setSelected(optionManager.getIsFullScreen());
+
+
+    TextFlow fullscreenText = new TextFlow(new Text("Fullscreen:"));
+    rightFullscreenContainer.getChildren().add(fullscreenButton);
+    leftFullscreenContainer.getChildren().add(fullscreenText);
+    fullscreenContainer.getChildren().addAll(leftFullscreenContainer, rightFullscreenContainer);
+
+
+    HBox.setHgrow(leftFullscreenContainer, Priority.ALWAYS);
+    HBox.setHgrow(rightFullscreenContainer, Priority.ALWAYS);
+    rightFullscreenContainer.setAlignment(Pos.CENTER_RIGHT);
+    leftFullscreenContainer.setAlignment(Pos.CENTER_LEFT);
+
+    menuContainer.getChildren().addAll(darkModeContainer, fullscreenContainer);
+
+
+    //Menu styling
+    menuContainer.getStyleClass().add("menu");
+
+    return menuContainer;
+  }
+
+  /**
+   * Builds the bottom menu.
+   *
+   * @return the bottom menu in a HBox.
+   */
+  private HBox buildBottomMenu() {
+
+    //Bottom menubar container
+    final HBox leftBox = new HBox();
+    final HBox rightBox = new HBox();
+    final HBox bottom = new HBox();
+
+    //Buttons
+    final Button returnButton = new Button("Return");
+    final Button saveButton = new Button("Save");
+
+    //Padding
+    leftBox.setPadding(new Insets(20, 0, 20, 20));
+    rightBox.setPadding(new Insets(20, 20, 20, 0));
+
+    returnButton.setOnMouseEntered(event -> ButtonUtility.buttonHover(returnButton));
+    saveButton.setOnMouseEntered(event -> ButtonUtility.buttonHover(saveButton));
+
+    returnButton.setOnMouseExited(event -> ButtonUtility.buttonExit(returnButton));
+    saveButton.setOnMouseExited(event -> ButtonUtility.buttonExit(saveButton));
+
+    //Button placement
+    leftBox.getChildren().add(returnButton);
+    rightBox.getChildren().add(saveButton);
+    bottom.getChildren().addAll(leftBox, rightBox);
+
+    //Node alignment
+    leftBox.setAlignment(Pos.CENTER_LEFT);
+    rightBox.setAlignment(Pos.CENTER_RIGHT);
+
+    //Node padding
+    HBox.setHgrow(leftBox, Priority.ALWAYS);
+    HBox.setHgrow(rightBox, Priority.ALWAYS);
+
+    saveButton.setOnAction(event -> {
+      controller.saveSettings(darkModeButton.isSelected(), fullscreenButton.isSelected());
+      Stage stage = (Stage) saveButton.getScene().getWindow();
+      stage.close();
+    });
+
+    returnButton.setOnAction(event -> {
+      Stage stage = (Stage) returnButton.getScene().getWindow();
+      stage.close();
+    });
+
+    //Styling
+    returnButton.getStyleClass().add("secondary-button");
+    saveButton.getStyleClass().add("secondary-button");
+
+    return bottom;
+  }
+
+
+}
 
