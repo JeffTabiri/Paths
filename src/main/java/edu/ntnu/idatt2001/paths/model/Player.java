4c668dc956438ee171040e0fa70eb14b59ec7258
Elementum <tabirijeffrey@gmail.com>
2023-05-22 15:05:05 +0200

    docs(player): add javadoc and reformat

--- src/main/java/edu/ntnu/idatt2001/paths/model/Player.java
+++ src/main/java/edu/ntnu/idatt2001/paths/model/Player.java
@@ -3,14 +3,15 @@ package edu.ntnu.idatt2001.paths.model;
 import java.util.List;
 
 /**
+ * <h1>Player</h1>
  * A {@code Player} class represents a player in the game.
  * It contains fields such as name, health, score and gold.
- * The attributes of the player can be accessed using the getters
+ * The attributes of the player can be accessed using the getters.
  * and changed using designated methods.
  *
  * @author Created by Jeffrey Yaw Annor Tabiri and Ari Maman
- * @version 06/02/2023
- * @since JDK 17.0.6
+ * @version 2.0
+ * @since 06/02/2023
  */
 public class Player {
   private final String name;
@@ -21,8 +22,6 @@ public class Player {
 
   /**
    * Constructor which is responsible for the creation of the player class.
-   *
-   * @throws IllegalArgumentException if health, score or gold is negative.
    */
   public Player(PlayerBuilder playerBuilder) {
     this.name = playerBuilder.name;
@@ -32,28 +31,55 @@ public class Player {
     this.inventory = playerBuilder.inventory;
   }
 
+  /**
+   * A method for getting the name of the {@code Player}.
+   *
+   * @return the name of the player. Returns a string.
+   */
   public String getName() {
     return name;
   }
 
+
+  /**
+   * A method for getting the health of the {@code Player}.
+   *
+   * @return the health of the player. Returns an integer.
+   */
   public int getHealth() {
     return health;
   }
 
+  /**
+   * A method for getting the score of the {@code Player}.
+   *
+   * @return the score of the player. Returns an integer.
+   */
   public int getScore() {
     return score;
   }
 
+  /**
+   * A method for getting the gold of the {@code Player}.
+   *
+   * @return the gold of the player. Returns an integer.
+   */
   public int getGold() {
     return gold;
   }
 
+
+  /**
+   * A method for getting the inventory of the {@code Player}.
+   *
+   * @return the inventory of the player. Returns a list of strings.
+   */
   public List<String> getInventory() {
     return inventory;
   }
 
   /**
-   * A method for adding health to the player.
+   * A method for adding health to the {@code Player}.
    *
    * @param health is the amount of health to be added. It is a positive integer.
    *
@@ -74,12 +100,17 @@ public class Player {
    * @throws IllegalArgumentException if health is negative.
    */
   public void removeHealth(int health) {
-    this.health += health;
+    if (this.health + health < 0) {
+      this.health = 0;
+    } else {
+      this.health += health;
+    }
+
   }
 
 
   /**
-   * A method for adding score to the player
+   * A method for adding score to the player.
    *
    * @param score is the amount of score to be added. It is a positive integer.
    *
@@ -94,29 +125,25 @@ public class Player {
     this.score += score;
   }
 
-    /**
-     * A method for removing score from the player
-     *
-     * @param score is the amount of score to be removed. It is a positive integer
-     *
-     * @throws IllegalArgumentException if score is negative
-     */
+  /**
+  * A method for removing score from the player.
+  *
+  * @param score is the amount of score to be removed. It is a positive integer
+  *
+  */
   public void removeScore(int score) {
 
-    if (score < 0) {
-      throw new IllegalArgumentException("The additional score cannot be negative");
+    if (this.score + score < 0) {
+      this.score = 0;
+    } else {
+      this.score += score;
     }
 
-    if (score > this.score) {
-      score = this.score;
-    }
-
-    this.score -= score;
   }
 
 
   /**
-   * A method for adding gold to the player
+   * A method for adding gold to the player.
    *
    * @param gold is the amount of gold to be added. It is a positive integer
    *
@@ -132,27 +159,23 @@ public class Player {
   }
 
   /**
-   * A method for removing gold from the player
+   * A method for removing gold from the player.
    *
    * @param gold is the amount of gold to be removed. It is a positive integer
    * @throws IllegalArgumentException if gold is negative or the player does not have enough gold
    */
   public void removeGold(int gold) {
 
-    if (gold < 0) {
-      throw new IllegalArgumentException("The additional gold cannot be negative");
-    }
-
-    if (gold > this.gold) {
+    if (this.gold + gold < 0) {
       throw new IllegalArgumentException("The player does not have enough gold");
     }
 
-    this.gold -= gold;
+    this.gold += gold;
   }
 
 
   /**
-   * A method for adding an item to the inventory
+   * A method for adding an item to the inventory.
    *
    * @param item is the instance to be added to the inventory
    * @throws IllegalArgumentException if the item is empty
@@ -168,7 +191,7 @@ public class Player {
 
 
   /**
-   * A method for removing an item from the players inventory
+   * A method for removing an item from the players inventory.
    *
    * @param item is the instance to be removed from the players inventory
    * @throws IllegalArgumentException if the item is empty or not in the inventory
@@ -188,7 +211,7 @@ public class Player {
 
 
   /**
-   * A toString method for the player class.
+   * A {@code toString}. method for the player class.
    * Shows the name, health, score, gold and inventory of the player.
    * Acts as a summary of the player.
    *
--- src/main/java/edu/ntnu/idatt2001/paths/model/PlayerBuilder.java
+++ src/main/java/edu/ntnu/idatt2001/paths/model/PlayerBuilder.java
@@ -3,60 +3,107 @@ package edu.ntnu.idatt2001.paths.model;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * <h1>PlayerBuilder</h1>
+ * {@code PlayerBuilder} is a builder class for the {@code Player} class.
+ * It is used to create a {@code Player} object with the desired attributes.
+ * A {@code PlayerBuilder} object is created with the name of the player.
+ */
 public class PlayerBuilder {
+  final String name;
+  int health;
+  int score;
+  int gold;
+  List<String> inventory = new ArrayList<>();
 
-    final String name;
-    int health;
-    int score;
-    int gold;
-    List<String> inventory = new ArrayList<>();
+  /**
+  * Constructor which is responsible for the creation of the {@code Player} class.
+  *
+  * @param name is the name of the player.
+  */
+  public PlayerBuilder(String name) {
+    this.name = name;
+  }
 
-    public PlayerBuilder(String name) {
-        this.name = name;
+  /**
+   * A builder method which is responsible for building the {@code Player} gold.
+   *
+   * @param health is the health of the {@code Player} object.
+   * @return the {@code PlayerBuilder} object with the health of the {@code Player} object.
+   */
+  public PlayerBuilder health(int health) {
+    this.health = health;
+    return this;
+  }
+
+  /**
+   * A builder method which is responsible for building the {@code Player} score.
+   *
+   * @param score is the score of the {@code Player} object.
+   * @return the {@code PlayerBuilder} object with the score of the {@code Player} object.
+   */
+  public PlayerBuilder score(int score) {
+    this.score = score;
+    return this;
+  }
+
+  /**
+   * A builder method which is responsible for the gold of the {@code Player} object.
+   *
+   * @param gold is the gold of the {@code Player} object.
+   * @return the {@code PlayerBuilder} object with the gold of the {@code Player} object.
+   */
+  public PlayerBuilder gold(int gold) {
+    this.gold = gold;
+    return this;
+  }
+
+  /**
+   * A builder method which is responsible for building the {@code Player} inventory.
+   *
+   * @param inventory is the inventory of the {@code Player} object.
+   *
+   * @return the {@code PlayerBuilder} object with the inventory of the {@code Player} object.
+   */
+  public PlayerBuilder inventory(List<String> inventory) {
+    this.inventory = inventory;
+    return this;
+  }
+
+  /**
+   * A method for building the {@code Player} object.
+   *
+   * @return the {@code Player} object as specified by the {@code PlayerBuilder} object.
+   */
+  public Player build() {
+    Player player = new Player(this);
+    validatePlayerBuilder(player);
+    return player;
+  }
+
+  /**
+   * A method for validating the {@code PlayerBuilder} object.
+   *
+   * @param player is the {@code Player} object to be validated.
+   * @throws IllegalArgumentException if health, score or gold is negative or if the name is empty.
+   *
+   */
+  private void validatePlayerBuilder(Player player) {
+    if (player.getHealth() <= 0) {
+      throw new IllegalArgumentException("Health cannot be negative");
     }
 
-    public PlayerBuilder health(int health) {
-        this.health = health;
-        return this;
+    if (player.getScore() < 0) {
+      throw new IllegalArgumentException("Score cannot be negative");
     }
 
-    public PlayerBuilder score(int score) {
-        this.score = score;
-        return this;
+    if (player.getGold() < 0) {
+      throw new IllegalArgumentException("Gold cannot be negative");
     }
 
-    public PlayerBuilder gold(int gold) {
-        this.gold = gold;
-        return this;
+    if (player.getName().isEmpty()) {
+      throw new IllegalArgumentException("The name cannot be empty");
     }
-
-    public PlayerBuilder inventory(List<String> inventory) {
-        this.inventory = inventory;
-        return this;
-    }
-
-    public Player build() {
-        Player player = new Player(this);
-        validatePlayerBuilder(player);
-        return player;
-    }
-
-    private void validatePlayerBuilder(Player player) {
-        if (player.getHealth() <= 0) {
-            throw new IllegalArgumentException("Health cannot be negative");
-        }
-
-        if (player.getScore() < 0) {
-            throw new IllegalArgumentException("Score cannot be negative");
-        }
-
-        if (player.getGold() < 0) {
-            throw new IllegalArgumentException("Gold cannot be negative");
-        }
-
-        if (player.getName().isEmpty()) {
-            throw new IllegalArgumentException("The name cannot be empty");
-        }
-    }
+  }
 
 }
