fec35a2b99c6e21901796367bc7810efa3aafd0f
Elementum <tabirijeffrey@gmail.com>
2023-05-22 15:03:49 +0200

    docs(choosestoryscene): add javadoc and reformat

--- src/main/java/edu/ntnu/idatt2001/paths/view/ChooseStoryView.java
+++ src/main/java/edu/ntnu/idatt2001/paths/view/ChooseStoryView.java
@@ -1,11 +1,14 @@
 package edu.ntnu.idatt2001.paths.view;
 
 import edu.ntnu.idatt2001.paths.controller.ChooseStoryController;
-import edu.ntnu.idatt2001.paths.model.OptionManager;
+import edu.ntnu.idatt2001.paths.enums.GameStates;
+import edu.ntnu.idatt2001.paths.model.manager.AudioManager;
+import edu.ntnu.idatt2001.paths.model.manager.OptionManager;
 import edu.ntnu.idatt2001.paths.utility.AlertUtility;
-import edu.ntnu.idatt2001.paths.utility.AudioEngine;
-import edu.ntnu.idatt2001.paths.utility.ButtonEffects;
-import edu.ntnu.idatt2001.paths.utility.GameStates;
+import edu.ntnu.idatt2001.paths.utility.ButtonUtility;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Logger;
 import javafx.animation.Animation;
 import javafx.animation.TranslateTransition;
 import javafx.collections.FXCollections;
@@ -17,68 +20,115 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-import java.io.File;
-import java.io.IOException;
 
+/**
+ * <h1>ChooseStoryView</h1>
+ * {@code ChooseStoryView} is the view class for the choose story view.
+ * This class is responsible for building the choose story view.
+ *
+ * @author Elementum
+ * @version 1.0
+ * @see ChooseStoryController
+ * @since 06/04/2023
+ */
 public class ChooseStoryView {
-
-  OptionManager optionManager = OptionManager.getInstance();
-
-  AudioEngine audioEngine = AudioEngine.getInstance();
-
-  StackPane view = new StackPane();
-
-  ListView<String> storyListView = new ListView<>();
-
+  Logger logger = Logger.getLogger(getClass().getName());
+  OptionManager optionManager;
+  AudioManager audioManager;
+  StackPane view;
+  ListView<String> storyListView;
   ChooseStoryController controller;
 
-  public ChooseStoryView(ChooseStoryController controller) {
-    storyListView.getStyleClass().add("file-view");
 
-    audioEngine.playMusic(GameStates.MAIN_MENU);
+  /**
+   * Constructor for the view. Responsible for building the view.
+   *
+   * @param controller the controller for the view.
+   */
+  public ChooseStoryView(ChooseStoryController controller) {
+    optionManager = OptionManager.getInstance();
+    audioManager = AudioManager.getInstance();
+    view = new StackPane();
+    storyListView = new ListView<>();
+    this.controller = controller;
+
+
+    audioManager.playMusic(GameStates.MAIN_MENU);
+
+    buildView();
+  }
+
+
+  /**
+   * Builds the title for the choose story scene.
+   */
+  private void buildView() {
+
+    //Initializing root
+    BorderPane root = new BorderPane();
+
+    //Building root
+    root.setTop(buildTitle());
+    root.setCenter(buildCenter());
+    root.setBottom(buildBottomMenu());
+
+    //Configuring root
+    root.setPadding(new Insets(10, 20, 10, 20));
+
+    //CSS styling
+    root.getStylesheets().add(optionManager.getCurrentStyleSheet());
+
+    //Building root
+    view.getChildren().addAll(buildPane(), root);
+
+  }
+
+  /**
+   * Builds the title for the choose story scene.
+   *
+   * @return the title in a HBox.
+   */
+  public ListView<String> buildCenter() {
 
     storyListView.setItems(FXCollections.observableArrayList(controller.getStoryList()));
+
+    Text placeholder = new Text("No stories found");
+
+    storyListView.setPlaceholder(placeholder);
 
     storyListView.setOnMouseClicked(event -> {
       if (event.getClickCount() == 2) {
         if (storyListView.getSelectionModel().getSelectedItem() == null) {
           AlertUtility.showErrorAlert("Error", "No story selected. Please select a story");
+          logger.warning("No story selected");
           return;
         }
         popupBox().show();
       }
     });
 
+    placeholder.getStyleClass().add("content");
+    storyListView.getStyleClass().add("file-view");
 
 
-    this.controller = controller;
 
-    buildView();
+    return storyListView;
   }
 
-
-  public void buildView() {
-
-    BorderPane root = new BorderPane();
-
-    root.setBottom(buildBottomMenu());
-    root.setTop(buildTitle());
-    root.setCenter(storyListView);
-    root.setPadding(new Insets(10, 20, 10, 20));
-
-    //CSS styling
-    root.getStylesheets().add(optionManager.getCurrentStyle());
-
-    view.getChildren().addAll(buildPane(), root);
-
-  }
-
+  /**
+   * Builds the popup box for the load view.
+   */
   public Parent asParent() {
     return view;
   }
@@ -92,21 +142,22 @@ public class ChooseStoryView {
   private HBox buildBottomMenu() {
 
     //Bottom menubar container
-    HBox leftBox = new HBox();
-    HBox rightBox = new HBox();
-    HBox bottom = new HBox();
+    final HBox leftBox = new HBox();
+    final HBox rightBox = new HBox();
+    final HBox bottom = new HBox();
 
     //Buttons
     Button returnButton = new Button("Return");
     Button startGameButton = new Button("Start Game");
 
     //Button actions
-    returnButton.setOnAction(e -> controller.goBackHandler());
+    returnButton.setOnAction(e -> controller.onActionGoBack());
 
     startGameButton.setOnAction(e -> {
 
       if (storyListView.getSelectionModel().getSelectedItem() == null) {
         AlertUtility.showErrorAlert("Error!", "No story selected. Please select a story");
+        logger.warning("No story selected");
         return;
       }
 
@@ -115,13 +166,12 @@ public class ChooseStoryView {
     });
 
     //Hover effects
-    returnButton.setOnMouseEntered(e -> ButtonEffects.buttonHover(returnButton));
-    startGameButton.setOnMouseEntered(e -> ButtonEffects.buttonHover(startGameButton));
+    returnButton.setOnMouseEntered(e -> ButtonUtility.buttonHover(returnButton));
+    startGameButton.setOnMouseEntered(e -> ButtonUtility.buttonHover(startGameButton));
 
     //Exit hover effects
-    returnButton.setOnMouseExited(e -> ButtonEffects.buttonExit(returnButton));
-    startGameButton.setOnMouseExited(e -> ButtonEffects.buttonExit(startGameButton));
-
+    returnButton.setOnMouseExited(e -> ButtonUtility.buttonExit(returnButton));
+    startGameButton.setOnMouseExited(e -> ButtonUtility.buttonExit(startGameButton));
 
     //Padding
     leftBox.setPadding(new Insets(20, 0, 20, 20));
@@ -148,6 +198,11 @@ public class ChooseStoryView {
   }
 
 
+  /**
+   * Builds the popup box for the choose story scene.
+   *
+   * @return the popup box in a Stage.
+   */
   private Pane buildPane() {
     Pane pane = new Pane();
 
@@ -160,13 +215,19 @@ public class ChooseStoryView {
     return pane;
   }
 
+
+  /**
+   * Builds the popup box for the choose story scene.
+   *
+   * @return the popup box in a Stage.
+   */
   private HBox buildTitle() {
 
     //Title text
     Text gameTitle = new Text("CHOOSE STORY");
 
     //Title container
-    HBox titleBox = new HBox();
+    final HBox titleBox = new HBox();
 
     //Image
     ImageView image = new ImageView("/images/UI/title/UI_Flat_Banner_01_Upward.png");
@@ -207,8 +268,13 @@ public class ChooseStoryView {
   }
 
 
-
+  /**
+  * Creates a popup box for the user to enter their name.
+  *
+  * @return the popup box
+  */
   private Stage popupBox() {
+
     final Stage dialog = new Stage();
 
     dialog.initModality(Modality.APPLICATION_MODAL);
@@ -238,38 +304,46 @@ public class ChooseStoryView {
     confirmButton.setOnAction(
             event -> {
 
-              ButtonEffects.buttonPressed(confirmButton);
+              ButtonUtility.buttonPressed();
 
               try {
 
                 String storyAddress = "src/main/resources/story/preloadedStory/";
                 String name = nameField.getText();
+                String ending = ".paths";
 
-                controller.startGameHandler(new File(storyAddress + storyListView.getSelectionModel().getSelectedItem()), name);
+                controller.startGameHandler(new File(
+                        storyAddress
+                                + storyListView.getSelectionModel().getSelectedItem() + ending),
+                        name);
 
                 dialog.close();
 
               } catch (IOException e) {
 
                 AlertUtility.showErrorAlert("xf:", e.getMessage());
+                logger.warning(e.getMessage());
 
               } catch (IllegalArgumentException e) {
 
-                AlertUtility.showErrorAlert("xsf", e.getMessage());
+                AlertUtility.showErrorAlert("Error", e.getMessage());
+                logger.warning(e.getMessage());
 
+              } catch (Exception e) {
+                AlertUtility.showErrorAlert("Unspecified Error", e.getMessage());
+                logger.warning(e.getMessage());
               }
 
             });
 
-    confirmButton.setOnMouseEntered(event -> ButtonEffects.buttonHover(confirmButton));
+    confirmButton.setOnMouseEntered(event -> ButtonUtility.buttonHover(confirmButton));
 
-    confirmButton.setOnMouseExited(event -> ButtonEffects.buttonExit(confirmButton));
+    confirmButton.setOnMouseExited(event -> ButtonUtility.buttonExit(confirmButton));
 
     //Styling
-    popupBox.getStylesheets().add("css/global.css");
+    popupBox.getStylesheets().add("css/Light.css");
     confirmButton.getStyleClass().add("secondary-button");
     popupTitle.getStyleClass().add("secondary-text");
-
 
     Scene popupScene = new Scene(popupBox, 300, 200);
 
@@ -278,8 +352,4 @@ public class ChooseStoryView {
     return dialog;
 
   }
-
-
-
-
 }
