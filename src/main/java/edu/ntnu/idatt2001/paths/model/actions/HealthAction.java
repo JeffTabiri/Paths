374f914cf962789ac95e6629fa8609570146774c
Elementum <tabirijeffrey@gmail.com>
2023-05-22 15:07:00 +0200

    docs(health): add javadoc and reformat

--- src/main/java/edu/ntnu/idatt2001/paths/model/actions/HealthAction.java
+++ src/main/java/edu/ntnu/idatt2001/paths/model/actions/HealthAction.java
@@ -3,15 +3,17 @@ package edu.ntnu.idatt2001.paths.model.actions;
 import edu.ntnu.idatt2001.paths.model.Player;
 
 /**
- * The class HealthAction represents the action of changing the health of a player.
- * The amount of health is specified in the constructor.
- * The class HealthAction implements the interface Action.
+ * <h1>HealthAction</h1>
+ * {@code HealthAction} represents the action of changing the health of a player.
+ * The amount of health is specified in the {@code HealthAction} constructor.
+ * The class HealthAction implements the interface {@code Action}.
  *
  * @author Created by Jeffrey Yaw Annor Tabiri and Ari Maman
- * @version 06/02/2023
- * @since JDK 17.0.6
+ * @version 1.0
+ * @since 06/02/2023
  */
 public class HealthAction implements Action {
+
   private final int health;
 
   /**
@@ -25,22 +27,34 @@ public class HealthAction implements Action {
   }
 
   /**
-   * The method execute adds the health to the player's health.
+   * The {@code execute} method adds changes the health of the player.
+   * If the health is negative, the player loses health.
+   * If the health is positive, the player gains health.
+   *
+   * @throws IllegalArgumentException if the player is null.
    */
   public void execute(Player player) {
 
-    if (health < 0) {
-      player.removeHealth(health);
+    if (player == null) {
+      throw new IllegalArgumentException("Player cannot be null");
     }
 
-    else if (health > 0) {
+    if (health < 0) {
+      player.removeHealth(health);
+    } else if (health > 0) {
       player.addHealth(health);
     }
 
   }
 
+  /**
+  * The method {@code toString} returns a representation of the {@code HealthAction}.
+  *
+  * @return a string representation of the {@code HealthAction}.
+  */
   @Override
   public String toString() {
     return "{Health}(" + health + ")";
   }
+
 }
--- src/main/java/edu/ntnu/idatt2001/paths/model/goals/HealthGoal.java
+++ src/main/java/edu/ntnu/idatt2001/paths/model/goals/HealthGoal.java
@@ -3,12 +3,12 @@ package edu.ntnu.idatt2001.paths.model.goals;
 import edu.ntnu.idatt2001.paths.model.Player;
 
 /**
- * An HealthGoal represents a final state to the player's health.
- * The player's health must be greater than or equal to the minimum health
+ * <h1>HealthGoal</h1>
+ * An {@code HealthGoal} represents a final state to the player's health that must be fulfilled.
  *
  * @author Created by Jeffrey Yaw Annor Tabiri and Ari Maman
- * @version 06/02/2023
- * @since JDK 17.0.6
+ * @version 1.0
+ * @since 06/02/2023
  */
 public class HealthGoal implements Goal {
 
@@ -17,21 +17,33 @@ public class HealthGoal implements Goal {
   /**
    * The constructor for the class {@code HealthGoal}.
    *
-   * @param minimumHealth is defined as the minimal health a player can have.
+   * @param minimumHealth is defined as the minimal health a
+   *                      player can have for the goal to be fulfilled.
    *                      Gets assigned in the constructor. Is an integer.
    *
+   * @throws IllegalArgumentException if the minimum health is negative.
    */
   public HealthGoal(int minimumHealth) {
+
+    if (minimumHealth < 0) {
+      throw new IllegalArgumentException("Minimum health cannot be negative");
+    }
+
     this.minimumHealth = minimumHealth;
   }
 
   /**
    * The method {@code isFulfilled} checks if the player has reached the goal.
    *
-   * @return true or false, which is checked upon the class field.
+   * @return true if the player has reached the goal, false if not.
    *
    */
   public boolean isFulfilled(Player player) {
+
+    if (player == null) {
+      throw new IllegalArgumentException("Player cannot be null");
+    }
+
     return player.getHealth() >= minimumHealth;
   }
 }
